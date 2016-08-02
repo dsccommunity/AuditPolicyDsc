6afe1fa4fda7a05a102f1891a5590e8878bd7849
adam.haynes <adam.haynes@outlook.com>
2016-08-02 14:26:30 +0200

    Start of review

---
+++ Tests/Integration/MSFT_AuditOption.config.ps1
@@ -0,0 +1,26 @@
+<#
+.Synopsis
+   DSC Configuration Template for DSC Resource Integration tests.
+.DESCRIPTION
+   To Use:
+     1. Copy to \Tests\Integration\ folder and rename MSFT_<ResourceName>.config.ps1 (e.g. MSFT_xFirewall.config.ps1)
+     2. Customize TODO sections.
+
+.NOTES
+#>
+
+configuration 'MSFT_AuditOption_config' {
+
+    Import-DscResource -ModuleName 'AuditPolicyDsc'
+
+    node localhost {
+
+        AuditOption Integration_Test 
+        {
+            Name  = $optionName
+            Value = $optionValue
+        }
+    }
+}
+
+# TODO: (Optional): Add More Configuration Templates
