9d50cf61b0e3eeb3473f0a3ec60522c27cc83f29
Zachary Alexander <zjalexander@users.noreply.github.com>
2016-08-08 10:51:01 -0700

    Adding examples for AuditCSV

---
+++ Examples/Example_AuditCsv.ps1
@@ -0,0 +1,17 @@
+﻿Configuration AuditPolicy
+{
+    Import-DscResource -ModuleName xAuditPolicy
+    node localhost
+    {
+        xAuditCsv auditPolicy
+        {
+            CsvPath = "C:\Users\Administrator\Documents\audit.csv"
+        }
+    
+    }
+}
+AuditPolicy
+
+invoke-dscresource xAuditCsv -Method Test -Property @{CsvPath = "C:\Users\Administrator\Documents\audit.csv"} -ModuleName xAuditPolicy -verbose
+invoke-dscresource xAuditCsv -Method Get -Property @{CsvPath = "C:\Users\Administrator\Documents\audit.csv"} -ModuleName xAuditPolicy -verbose
+Start-DscConfiguration -Wait -verbose -path .\AuditPolicy -force
