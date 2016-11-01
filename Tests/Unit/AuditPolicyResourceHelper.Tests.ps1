a9075b3de5407886af5874f707ace88ae54043cd
adam.haynes <adam.haynes@outlook.com>
2016-11-01 13:32:28 +0100

    added reviewable feedback

--- Tests/Unit/AuditPolicyResourceHelper.Tests.ps1
+++ Tests/Unit/AuditPolicyResourceHelper.Tests.ps1
@@ -1,182 +1,163 @@
 #requires -RunAsAdministrator
 
 # get the root path of the resourse
-[String] $moduleRoot = Split-Path -Parent ( Split-Path -Parent $PSScriptRoot ) 
+[String] $moduleRoot = Split-Path -Parent ( Split-Path -Parent $PSScriptRoot )
 
 # get the module name to import
-[string] $sut = ( Split-Path -Leaf $MyInvocation.MyCommand.Path )  -Replace "\.tests\.ps1", ".psm1"
+[String] $systemUnderTest = ( Split-Path -Leaf $MyInvocation.MyCommand.Path ) `
+                                -Replace "\.tests\.ps1", ".psm1"
 
-Import-Module "$moduleRoot\DSCResources\$sut" -Force
-
+Import-Module -Name (Join-Path -Path ( Join-Path -Path $moduleRoot -ChildPath 'DSCResources') `
+                               -ChildPath $systemUnderTest ) `
+                               -Force
 #region Generate data
 
 <# 
-    The auditpol utility outputs the list of categories and subcategories in a couple 
-    of different ways and formats. Using the /list flag only returns the categories 
-    without the associated audit setting, so it is easier to filter on.
+    The auditpol utility outputs the list of categories and subcategories in a couple of different 
+    ways. Using the /list flag only returns the categories without the associated audit setting, 
+    so it is easier to filter later on.
 #>
 
-function Get-AuditpolCategories
-{
-    $auditpol = auditpol /list /subcategory:*
-    $Categories = @()
-    $SubCategories = @()
-    $auditpol | Where-Object {$_ -notlike 'Category/Subcategory*'} | ForEach-Object `
-    {
-        # the categories do not have any space in front of them, but the subcategories do.
-        if ( $_ -notlike " *" )
-        {
-            $Categories += $_.Trim()
-        }
-        else
-        {
-            $SubCategories += $_.trim()
-        }
-    } 
-    $Categories, $SubCategories
-} 
+$categories = @()
+$subcategories = @()
 
-$Categories, $SubCategories = Get-AuditpolCategories
+auditpol /list /subcategory:* | 
+Where-Object { $_ -notlike 'Category/Subcategory*' } | ForEach-Object `
+{
+    # The categories do not have any space in front of them, but the subcategories do.
+    if ( $_ -notlike " *" )
+    {
+        $categories += $_.Trim()
+    }
+    else
+    {
+        $subcategories += $_.trim()
+    }
+} 
 
 #endregion
 
-Describe 'Prereq' {
+Describe 'Prerequisites' {
 
-    # There are several dependencies for both Pester and auditpol resource that need to be validated.
-
-    It "Checks if the tests are running as admin" {
-
-        # the tests need to run as admin to have access to the auditpol data
+    # There are several dependencies for both Pester and AuditPolicyDsc that need to be validated.
+    It "Should be running as admin" {
+        # The tests need to run as admin to have access to the auditpol data
         ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
         [Security.Principal.WindowsBuiltInRole] "Administrator") | Should Be $true
     }
 
-    It "Checks auditpol.exe exists in System32  " {
-
-        # if the auditpol is not located on the system, the entire module will fail
+    It "Should find auditpol.exe in System32" {
+        # If the auditpol is not located on the system, the entire module will fail
         Test-Path "$env:SystemRoot\system32\auditpol.exe" | Should Be $true
     }
 }
 
 Describe 'auditpol.exe output' {
 
-    # verify the raw auditpol output format has not changed across different OS versions and types.
-    
-    It "Checks auditpol default return with no parameters      " {
-
-        ( auditpol.exe )[0] | should BeExactly 'Usage: AuditPol command [<sub-command><options>]'
+    # Verify the raw auditpol output format has not changed across different OS versions and types.
+    It 'Should get auditpol default return with no parameters' {
+        ( auditpol.exe )[0] | Should BeExactly 'Usage: AuditPol command [<sub-command><options>]'
     }
 
-    It "Checks auditpol CSV header format with the /r switch   " {
-
+    It 'Should get CSV format with the /r switch' {
         ( auditpol.exe /get /subcategory:logon /r )[0] | 
-        should BeExactly "Machine Name,Policy Target,Subcategory,Subcategory GUID,Inclusion Setting,Exclusion Setting"
+        Should BeExactly "Machine Name,Policy Target,Subcategory,Subcategory GUID,Inclusion Setting,Exclusion Setting"
     }
 
-    # loop through the raw output of every category option to validate the auditpol /category subcommand
-    Foreach( $Category in $Categories ) 
+    # Loop through the raw output of every category option to validate the auditpol /category subcommand
+    Foreach( $category in $categories ) 
     {
-        Context "Category: $Category" {
+        Context "Category: $category" {
         
-            $auditpolCategory = auditpol.exe /get /category:$Category /r
+            $auditpolCategory = auditpol.exe /get /category:$category /r
           
-            It "Checks auditpol category returns empty string on line 1 " {
+            It 'Should return an empty string on line 1' {
 
-                $auditpolCategory[1] | should Be ""
+                $auditpolCategory[1] | Should Be ""
             }
 
-            It "Checks auditpol category returns first entry  on line 2 " {
+            It 'Should return the category contents on line 2' {
 
                 # the auditpol /r output starts with the computer name on each entry
-                $auditpolCategory[2] | should Match "$env:ComputerName"
+                $auditpolCategory[2] | Should Match "$env:ComputerName"
             }
         }
     }
 
-    # loop through the filtered output of every category option to validate the auditpol /category subcommand
-    Foreach( $Category in $Categories ) 
+    # Loop through the filtered output of every category option to validate the auditpol /category subcommand
+    Foreach( $category in $categories ) 
     {
-        Context "Category: $Category Filtered 'Select-String -Pattern `$env:ComputerName'" {
-        
+        Context "Category: $category Filtered 'Select-String -Pattern `$env:ComputerName'" {
             # Reuse the same command as the raw output context, only this time filter out the entries.
-            # this is to verify the row indexing is not broken in later formatting actions
-            $auditpolCategory = auditpol.exe /get /category:$Category /r | 
-            Select-String -Pattern $env:ComputerName
+            # This is to verify the row indexing is not broken in later formatting actions
+
+            $auditpolCategory = auditpol.exe /get /category:$category /r | 
+                Select-String -Pattern $env:ComputerName
             $auditpolCategoryCount = ($auditpolCategory | Measure-Object).Count
         
-            It "Checks auditpol category returns $auditpolCategoryCount items  " { 
-
-                # The header row has been stripped, so greater than 1 is required to account for multiple subcategories
-                $auditpolCategoryCount | should BeGreaterThan 1
+            It 'Should return more than one item' { 
+                # The header row has been stripped, so there should be more than one category to 
+                # account for multiple subcategories
+                $auditpolCategoryCount | Should BeGreaterThan 1
             }
 
-            # loop through the subcategories returned by the current category that was queried
-            for ( $i=0;$i -lt $auditpolCategoryCount;$i++ )
+            # Loop through the subcategories returned by the current category that was queried
+            for ( $i = 0; $i -lt $auditpolCategoryCount; $i++ )
             {
-                It "Checks auditpol category returns entry on line $i " {
-
+                It "Should return a subcategory on line $i" {
                     # Verify that each filtered row that is returned, is in the expected format 
-                    $auditpolCategory[$i] | should Match "$env:ComputerName,System,"                   # <- Add more specific regexto account for positional GUID
+                    $auditpolCategory[$i] | Should Match "$env:ComputerName,System,"
                 }
             }
 
-            It "Checks auditpol category returns `$null on line $auditpolCategoryCount " {
-
-                # with a zero base, the count of the subcategories should index to the end of the list
-                $auditpolCategory[$auditpolCategoryCount] | should BeNullOrEmpty
+            It 'Should return a null on the last line' {
+                # With a zero base, the count of the subcategories should index to the end of the list
+                $auditpolCategory[$auditpolCategoryCount] | Should BeNullOrEmpty
             }
         }
     }
 
-    # loop through the raw output of every subcategory to validate the auditpol /subcategory subcommand
-    Foreach( $Subcategory in $Subcategories ) 
+    # Loop through the raw output of every subcategory to validate the auditpol /subcategory subcommand
+    Foreach( $subcategory in $subcategories ) 
     {
-        Context "Subcategory: $Subcategory" {
+        Context "Subcategory: $subcategory" {
 
-            $auditpolSubcategory = auditpol.exe /get /subcategory:$Subcategory /r
+            $auditpolSubcategory = auditpol.exe /get /subcategory:$subcategory /r
 
-            It "Checks auditpol Subcategory returns empty string on line 1 " {
-
-                # verify the raw auditpol CSV header format has not changed across different OS versions and types. 
-                $auditpolSubcategory[1] | should BeNullOrEmpty
+            It 'Should return an empty string on line 1there should be more than one category to account for multiple subcategories' {
+                # Verify the raw auditpol CSV header format has not changed across different OS versions and types. 
+                $auditpolSubcategory[1] | Should BeNullOrEmpty
             }
         
-            It "Checks auditpol Subcategory returns first entry  on line 2 " {
-
-                # verify the raw auditpol CSV header format has not changed across different OS versions and types. 
-                $auditpolSubcategory[2] | should Match "$env:ComputerName"
+            It 'Should return the subcategory on line 2' {
+                # Verify the raw auditpol CSV header format has not changed across different OS versions and types. 
+                $auditpolSubcategory[2] | Should Match "$env:ComputerName"
             }
-
-            # add a regex for the entire string format to get an exact answer
         }
     }
 
-    # loop through the filtered output of every subcategory to validate the auditpol /subcategory subcommand
-    Foreach( $Subcategory in $Subcategories ) 
+    # Loop through the filtered output of every subcategory to validate the auditpol /subcategory subcommand
+    Foreach( $subcategory in $subcategories ) 
     {
-        Context "Subcategory: $Subcategory Filtered 'Select-String -Pattern `$env:ComputerName'" {
-        
+        Context "Subcategory: $subcategory Filtered 'Select-String -Pattern `$env:ComputerName'" {
             # Reuse the same command as the raw output context, only this time filter out the entries.
-            # this is to verify the row indexing is not broken in the formatting function
-            $auditpolSubcategory = auditpol.exe /get /subcategory:$Subcategory /r | 
-            Select-String -Pattern $env:ComputerName
+            # This is to verify the row indexing is not broken in the formatting function
+            $auditpolSubcategory = auditpol.exe /get /subcategory:$subcategory /r | 
+                Select-String -Pattern $env:ComputerName
 
-            It "Checks auditpol Subcategory returns one item         " {
-
-                # verify the raw auditpol CSV header format has not changed across different OS versions and types. 
-                ($auditpolSubcategory | Measure-Object).Count | should Be 1
+            It 'Should return a single subcategory' {
+                # Verify the raw auditpol CSV header format has not changed across different OS versions and types. 
+                ($auditpolSubcategory | Measure-Object).Count | Should Be 1
             }
 
-            It "Checks auditpol Subcategory returns entry on line 0  " {
-
-                    # Verify that each filtered row that is returned, is in the expected format 
-                    $auditpolSubcategory[0] | should Match "$env:ComputerName,System,"                   # <- Add more specific regex
+            It 'Should return the Subcategory on line 0' {
+                # Verify that each filtered row that is returned is in the expected format 
+                $auditpolSubcategory[0] | Should Match "$env:ComputerName,System,"
            }
 
-            It "Checks auditpol Subcategory returns `$null on line 1  " {
-
-                # verify the raw auditpol CSV header format has not changed across different OS versions and types. 
-                $auditpolSubcategory[1]| should BeNullOrEmpty
+            It 'Should return null on line 1' {
+                # Verify the raw auditpol CSV header format has not changed across different OS versions and types. 
+                $auditpolSubcategory[1]| Should BeNullOrEmpty
             }
         }
     }
@@ -186,54 +167,42 @@ Describe "Private function Invoke-Auditpol" {
 
     InModuleScope AuditPolicyResourceHelper {
 
-        $command = Get-Command Invoke-Auditpol
-        # parameters listed in the Hashtable name = type, Mandatory, validateSet
-        $parameters = @{
-                            'Command'    = @('String',@('set','get'))
-                            'Subcommand' = @('String[]')
-                        }
+        $command = Get-Command -Name Invoke-Auditpol
 
-        It "Should Exist" {
-
-            $command | Should Be $command 
+        It "Should find command Invoke-Auditpol" {
+            $command.Name | Should Be 'Invoke-Auditpol' 
         }
 
-        It 'With output type "String"' {
-
+        It 'Should output a type "String"' {
             $command.OutputType | Should Be 'System.String'
         }
 
-        # this test verifes that the /r switch is passed to auditpol 
-        It 'In a CSV format' {
+        It 'Should have a parameter Command' {
+            $command.Parameters.Command | Should Not BeNullOrEmpty
+        }
 
+        It 'Should have a ValidSet (set|get)' {
+            $command.Parameters.Command.Attributes.ValidValues | Should Be @('set','get')
+        }
+
+        It 'Should have a parameter Subcommand' {
+            $command.Parameters.Subcommand | Should Not BeNullOrEmpty
+        }
+
+        # These tests verify that the /r switch is passed to auditpol 
+        It 'Should return a CSV format when a single word subcategory is passed in' {
             ( Invoke-Auditpol -Command "Get" -SubCommand "Subcategory:Logoff" )[0] | 
             Should match ".,."
         }
 
-        # verify all of ther parameters are correct
-        foreach( $parameter in $parameters.GetEnumerator() )
-        {
-            $name = $parameter.key
-            It "Has a parameter '$name'" {
+        It 'Should return a CSV format when a multi-word subcategory is passed in' {
+            ( Invoke-Auditpol -Command "Get" -SubCommand "Subcategory:""Credential Validation""" )[0] | 
+            Should match ".,."
+        }
 
-                $command.Parameters[$name].Name | Should Be $name
-            }
-
-            $type = $parameter.value[0]
-            It "Of type '$type'" {
-
-                $command.Parameters[$name].ParameterType | Should Be $type
-            }
-
-            $validateSet = $parameter.value[1]
-
-            if ( $validateSet -ne $null )
-            {
-                It "With a validateSet '$validateSet'" {
-
-                    $command.Parameters[$name].Attributes.ValidValues | Should Be $validateSet
-                }
-            }
+        It 'Should return a CSV format when a option is passed in' {
+            ( Invoke-Auditpol -Command "Get" -SubCommand "option:CrashOnAuditFail" )[0] | 
+            Should match ".,."
         }
     }
 }
