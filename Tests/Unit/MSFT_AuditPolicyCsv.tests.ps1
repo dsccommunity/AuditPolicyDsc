2e89384258987f1b0016c1399cec4a4855e4430e
adam.haynes <adam.haynes@outlook.com>
2016-11-16 08:40:10 +0100

    Updated Get tests

--- Tests/Unit/MSFT_AuditPolicyCsv.tests.ps1
+++ Tests/Unit/MSFT_AuditPolicyCsv.tests.ps1
@@ -28,16 +28,23 @@ try
     InModuleScope $script:DSCResourceName {
 
         $testParameters = @{
-            CSVPath  = 'C:\temp\backup.csv'
+            CSVPath  = 'C:\temp\testBackup.csv'
         }
 
-        $TestCSV = Join-Path -Path $moduleRoot -ChildPath "Examples\test.csv"
-        $DifferentCSV = Join-Path -Path $moduleRoot -ChildPath "Examples\different.csv"
+        # Create the auditpol backup file to use in testing. 
+        @(@("Machine Name,Policy Target,Subcategory,Subcategory GUID,Inclusion Setting,Exclusion Setting,Setting Value")
+        @(",System,IPsec Driver,{0CCE9213-69AE-11D9-BED3-505054503030},Failure,,2")
+        @(",System,System Integrity,{0CCE9212-69AE-11D9-BED3-505054503030},Success,,1")
+        @(",System,Security System Extension,{0CCE9211-69AE-11D9-BED3-505054503030},No Auditing,,0")
+        @(",,Option:CrashOnAuditFail,,Disabled,,0")
+        @(",,RegistryGlobalSacl,,,,")) | Out-File $testParameters.CSVPath -Encoding utf8 -Force
 
         Describe "$($script:DSCResourceName)\Get-TargetResource" {
 
-            Mock -CommandName Invoke-SecurityCmdlet -MockWith { 
-                return $testParameters.CsvPath } -ModuleName MSFT_AuditPolicyCsv -Verifiable
+            $tempCsv = ([system.IO.Path]::GetTempFileName()).Replace('.tmp','.csv')
+
+            Mock -CommandName Invoke-SecurityCmdlet -MockWith { return $tempCsv } `
+                 -ModuleName MSFT_AuditPolicyCsv -Verifiable
                     
             It 'Should not throw an exception' {
                 { $script:getTargetResourceResult = Get-TargetResource @testParameters } | 
@@ -45,7 +52,7 @@ try
             }
 
             It 'Should return the correct hashtable property' {
-                $script:getTargetResourceResult.CSVPath | Should match ".csv"
+                $script:getTargetResourceResult.CSVPath | Should Be $tempCsv
             }
 
             It 'Should call expected Mocks' {    
@@ -56,23 +63,30 @@ try
 
         Describe "$($script:DSCResourceName)\Test-TargetResource" {
 
-            # mock call to the helper module to isolate Get-TargetResource
-            Mock Invoke-SecurityCmdlet { param($Action, $Path)}
-                                 
-            It "Returns an Object of type Boolean" {
-                Copy-Item -Path $TestCSV -Destination C:\Temp\test.csv -Force 
-                $result = Test-TargetResource -CSVPath $TestCSV
-                ($result -is [bool]) | Should Be $true
+            Mock -CommandName Invoke-SecurityCmdlet -MockWith { param($Action, $Path)}
+
+            Context 'CSVs are different' {
+
+                It 'Should not throw an exception' {
+                    { $script:testTargetResourceResult = Test-TargetResource @testParameters } | 
+                        Should Not Throw
+                }            
+
+                It 'Should return false' {
+                    $script:testTargetResourceResult | Should Be $false
+                }
             }
 
-            It "Returns False when CSVs are different" {
-                Copy-Item -Path $TestCSV -Destination C:\Temp\test.csv -Force 
-                (Test-TargetResource -CSVPath $DifferentCSV) | Should Be $False
-            }
+            Context 'CSVs are the same' {
+                
+                It 'Should not throw an exception' {
+                    { $script:testTargetResourceResult = Test-TargetResource @testParameters } | 
+                        Should Not Throw
+                }            
 
-            It "Returns True when CSVs are the same" {
-                Copy-Item -Path $TestCSV -Destination C:\Temp\test.csv -Force 
-                (Test-TargetResource -CSVPath $TestCSV) | Should Be $True
+                It 'Should return true' {
+                    $script:testTargetResourceResult | Should Be $true
+                }
             }
         }
 
@@ -90,6 +104,7 @@ try
         Describe 'Function Invoke-SecurityCmdlet' {
             
             Context 'Seucrity cmdlets are available' {
+                
                 Mock Get-Module -ParameterFilter {Name -eq "SecurityCmdlets"} `
                                 -MockWith {"SecurityCmdlets"} -Verifiable
                 Mock Restore-AuditPolicy -MockWith {}
